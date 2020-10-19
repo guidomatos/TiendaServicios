605cffd4c380767a0b5c981acb2dae1b11b6dc4b
guidomatos <guido.matos.88@gmail.com>
2020-10-18 22:45:42 -0500

    Code First - Migration con SQL Server

--- TiendaServicios.Api.Libro/Startup.cs
+++ TiendaServicios.Api.Libro/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TiendaServicios.Api.Libro.Persistencia;
 
 namespace TiendaServicios.Api.Libro
 {
@@ -25,6 +27,11 @@ namespace TiendaServicios.Api.Libro
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ContextoLibreria>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("ConexionDB"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
