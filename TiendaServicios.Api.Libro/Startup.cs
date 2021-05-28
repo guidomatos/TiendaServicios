66435d01d56a0ff7f1c1ac86e4ad5a8c486173d5
guidomatos <guido.matos.88@gmail.com>
2021-05-27 21:54:08 -0500

    Controller para consulta tipo Get

--- TiendaServicios.Api.Libro/Startup.cs
+++ TiendaServicios.Api.Libro/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using FluentValidation.AspNetCore;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
@@ -37,6 +38,8 @@ namespace TiendaServicios.Api.Libro
             });
 
             services.AddMediatR(typeof(Nuevo.Manejador).Assembly);
+
+            services.AddAutoMapper(typeof(Consulta.Ejecuta));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
