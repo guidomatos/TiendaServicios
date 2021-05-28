10e1550580f1d608365f67003531786fbf46890a
guidomatos <guido.matos.88@gmail.com>
2021-05-27 21:50:13 -0500

    Crear clase Aplicacion para consulta de registros

--- TiendaServicios.Api.Libro/Aplicacion/LibroMaterialDto.cs
+++ TiendaServicios.Api.Libro/Aplicacion/LibroMaterialDto.cs
@@ -7,5 +7,9 @@ namespace TiendaServicios.Api.Libro.Aplicacion
 {
     public class LibroMaterialDto
     {
+        public Guid? LibreriaMaterialId { get; set; }
+        public string Titulo { get; set; }
+        public DateTime? FechaPublicacion { get; set; }
+        public Guid? AutorLibro { get; set; }
     }
 }
