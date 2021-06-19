7a45d03236ea7538bb4efee3db8b827993abf1c8
guidomatos <guido.matos.88@gmail.com>
2021-06-19 17:14:51 -0500

    Creacion de Mocks y Meetodos de Unit Testing

--- TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -1,4 +1,8 @@
+using AutoMapper;
+using Moq;
 using System;
+using TiendaServicios.Api.Libro.Aplicacion;
+using TiendaServicios.Api.Libro.Persistencia;
 using Xunit;
 
 namespace TiendaServicios.Api.Libro.Tests
@@ -8,6 +12,22 @@ namespace TiendaServicios.Api.Libro.Tests
         [Fact]
         public void GetLibros()
         {
+            // que metodo dentro de mi microservice libro se esta encargando
+            // de realizar la consulta de libros de la base de datos ??
+            // 1. Emular a la instancia de entity framework core - ContextoLibreria
+            // pára emular las acciones y eventos de un objeto en un ambiente de unit test
+            // usaremos un objeto de Mock
+
+            var mockContexto = new Mock<ContextoLibreria>();
+
+            // 2. Emular al mapping IMapper
+
+            var mockMapper = new Mock<IMapper>();
+
+            // 3. Instanciar a la clase Manejador y pasarle como parametro los metodos
+
+            Consulta.Manejador manejador = new Consulta.Manejador(mockContexto.Object, mockMapper.Object);
+
 
         }
     }
