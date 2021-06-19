c93f8c4a754c1bdd14b5a3f5c47e0635cbf32017
guidomatos <guido.matos.88@gmail.com>
2021-06-19 17:57:13 -0500

    Unit Test Debugger y Ejecucion

--- TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -37,27 +37,45 @@ namespace TiendaServicios.Api.Libro.Tests
             dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.Expression).Returns(dataPrueba.Expression);
             dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.ElementType).Returns(dataPrueba.ElementType);
             dbSet.As<IQueryable<LibreriaMaterial>>().Setup(x => x.GetEnumerator()).Returns(dataPrueba.GetEnumerator());
+
+            dbSet.As<IAsyncEnumerable<LibreriaMaterial>>().Setup(x => x.GetAsyncEnumerator(new System.Threading.CancellationToken()))
+                .Returns(new AsyncEnumerator<LibreriaMaterial>(dataPrueba.GetEnumerator()));
+
+            var contexto = new Mock<ContextoLibreria>();
+            contexto.Setup(x => x.LibreriaMaterial).Returns(dbSet.Object);
+
+            return contexto;
         }
 
         [Fact]
-        public void GetLibros()
+        public async void GetLibros()
         {
+            System.Diagnostics.Debugger.Launch();
             // que metodo dentro de mi microservice libro se esta encargando
             // de realizar la consulta de libros de la base de datos ??
             // 1. Emular a la instancia de entity framework core - ContextoLibreria
             // pára emular las acciones y eventos de un objeto en un ambiente de unit test
             // usaremos un objeto de Mock
 
-            var mockContexto = new Mock<ContextoLibreria>();
+            var mockContexto = CrearContexto();
 
             // 2. Emular al mapping IMapper
 
-            var mockMapper = new Mock<IMapper>();
+            var mapConfig = new MapperConfiguration(cfg =>
+           {
+               cfg.AddProfile(new MappingTest());
+           });
 
+            var mapper = mapConfig.CreateMapper();
             // 3. Instanciar a la clase Manejador y pasarle como parametro los metodos
 
-            Consulta.Manejador manejador = new Consulta.Manejador(mockContexto.Object, mockMapper.Object);
+            Consulta.Manejador manejador = new Consulta.Manejador(mockContexto.Object, mapper);
 
+            Consulta.Ejecuta request = new Consulta.Ejecuta();
+
+            var lista = await manejador.Handle(request, new System.Threading.CancellationToken());
+
+            Assert.True(lista.Any());
 
         }
     }
--- TiendaServicios.Api.Libro/Persistencia/ContextoLibreria.cs
+++ TiendaServicios.Api.Libro/Persistencia/ContextoLibreria.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TiendaServicios.Api.Libro.Modelo;
 
 namespace TiendaServicios.Api.Libro.Persistencia
 {
     public class ContextoLibreria: DbContext
     {
+        public ContextoLibreria()
+        {
+
+        }
         public ContextoLibreria(DbContextOptions<ContextoLibreria> options) : base(options) { }
 
-        public DbSet<LibreriaMaterial> LibreriaMaterial { get; set; }
+        public virtual DbSet<LibreriaMaterial> LibreriaMaterial { get; set; }
     }
 }
