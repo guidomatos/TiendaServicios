6477591b18969b6193666d044fc39f3fa2da6ce7
guidomatos <guido.matos.88@gmail.com>
2021-06-20 17:41:19 -0500

    Creacion de proyecto libreria Net Core EnviarEmail

---
+++ TiendaServicios.Mensajeria.Email/SendGridLibreria/Implement/SendGridEnviar.cs
@@ -0,0 +1,34 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+using TiendaServicios.Mensajeria.Email.SendGridLibreria.Interface;
+using TiendaServicios.Mensajeria.Email.SendGridLibreria.Modelo;
+
+namespace TiendaServicios.Mensajeria.Email.SendGridLibreria.Implement
+{
+    public class SendGridEnviar : ISendGridEnviar
+    {
+        public async Task<(bool resultado, string errorMessage)> EnviarEmail(SendGridData data)
+        {
+            try
+            {
+                var sendGridCliente = new SendGridClient(data.SendGridAPIKey);
+                var destinatario = new EmailAddress(data.EmailDestinatario, data.NombreDestinatario);
+                var tituloEmail = data.Titulo;
+                var sender = new EmailAddress("guido.matos.88@gmail.com", "Guido Matos");
+                var contenidoMensaje = data.Contenido;
+
+                var objMensaje = MailHelper.CreateSingleEmail(sender, destinatario, tituloEmail, contenidoMensaje, contenidoMensaje);
+
+                await sendGridCliente.SendEmailAsync(objMensaje);
+
+                return (true, null);
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+    }
+}
---
+++ TiendaServicios.Mensajeria.Email/SendGridLibreria/Interface/ISendGridEnviar.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using TiendaServicios.Mensajeria.Email.SendGridLibreria.Modelo;
+
+namespace TiendaServicios.Mensajeria.Email.SendGridLibreria.Interface
+{
+    public interface ISendGridEnviar
+    {
+        Task<(bool resultado, string errorMessage)> EnviarEmail(SendGridData data);
+    }
+}
